cc0db80749bf3a36962ab5768e3d65d51fa15a5d
RobertoMoura1 <robertomoura.ads@gmail.com>
2026-03-31 23:03:18 -0300

    Fix PIX key typing for donations

--- src/lib/pix.ts
+++ src/lib/pix.ts
@@ -1,12 +1,18 @@
-export const pixKeys = [
+export type PixKey = {
+    type: "E-mail" | "CNPJ";
+    key: string;
+    icon: "mail" | "building";
+};
+
+export const pixKeys: PixKey[] = [
     {
         type: "E-mail",
         key: "saopiox@arquidiocesecampinas.com",
-        icon: "mail" as const,
+        icon: "mail",
     },
     {
         type: "CNPJ",
         key: "44588960/0014-04",
-        icon: "building" as const,
+        icon: "building",
     },
 ];
